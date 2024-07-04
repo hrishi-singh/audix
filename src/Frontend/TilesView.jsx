77d196fd8fa2a364262104ad4d897148ff300a3e
hrishi-singh <singh.123@iitj.ac.in>
2024-07-04 17:16:53 +0530

    removing sort feature due to errors and adding .eslintrc

--- src/Frontend/TilesView.jsx
+++ src/Frontend/TilesView.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import Cards from "./Cards";
 import "./style/tilesview.css";
-import { useEffect,useState,} from "react";
+// import { useEffect,useState,} from "react";
 function Tilesview(props) {
-const [sortValue,setSortValue]=useState();
-
-useEffect(() => {
-  console.log(sortValue);
-  if(props.data && sortValue==="sales2"){
-    // console.log("Going into sort",sortValue);
-    props.data.sort(function(a, b){return a.sales-b.sales});
-    console.log("sorted lowest to highest");
-  }
-  if(props.data && sortValue==="sales1"){
-    // console.log("Going into Highest sort",sortValue);
+// const [sortValue,setSortValue]=useState();
+// useEffect(() => {
+//   console.log(sortValue);
+//   if((props.data) && sortValue==="sales2"){
+//     (props.data).sort(function(a, b){return a.sales-b.sales});
+//     console.log("sorted lowest to highest");
+//   }
+//   if(props.data && sortValue==="sales1"){
+//     // console.log("Going into Highest sort",sortValue);
     
-    props.data.sort(function(a, b){return b.sales-a.sales});
-    console.log("sorted highest to lowest");
-  }
-  // setData(props.data);
-}, [sortValue]);
+//     props.data.sort(function(a, b){return b.sales-a.sales});
+//     console.log("sorted highest to lowest");
+//   }
+//   // setData(props.data);
+// }, [sortValue]);
 
   return (
     <div id="__tiles-container">
       <h4>{props.title}</h4>
-      <div className="__sort-filter-container">
+      {/* <div className="__sort-filter-container">
         <div className="__sort-container">
           <form action="#">
             <label htmlFor="sort">Sort by:</label>
@@ -36,7 +34,7 @@ useEffect(() => {
             </select>
           </form>
         </div>
-      </div>
+      </div> */}
       <div className="tiles">
         {props.data.map((det) => {
           return (
