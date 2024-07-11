523b212c06c35db82206898f9668af3e5255e848
hrishi-singh <singh.123@iitj.ac.in>
2024-07-11 18:31:09 +0530

    DB_URI updated

--- Backend/src/db/conn.js
+++ Backend/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.DATA_API_URL || "mongodb://127.0.0.1:27017/audiobook_database")
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/audiobook_database")
   .then(() => {
     console.log("connection successful");
   })
